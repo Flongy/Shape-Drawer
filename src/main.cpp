21681b890bc17e24da5354f8ed353791415c1b6e
Flongy <craftbymax@gmail.com>
2021-09-15 17:09:04 +0300

    Memory leak check
    Track how much memory is allocated via overloading the new and delete operators.
    
    To check current memory usage you can press M key. And after closing the window the usage is also printed.

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,8 @@
 #include "Shapes.h"
 #include "Scene.h"
 
+#include "MemoryCheck.h"
+
 
 constexpr uint32_t INITIAL_WIDTH = 1280;
 constexpr uint32_t INITIAL_HEIGHT = 720;
@@ -55,6 +57,10 @@ void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods
 		// Close application on pressing ESCAPE button
 		glfwSetWindowShouldClose(window, GLFW_TRUE);
 		return;
+	case GLFW_KEY_M:
+		if(action == GLFW_PRESS)
+			printMemoryUsage();
+		return;
 	default:
 		return;
 	}
@@ -63,89 +69,95 @@ void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods
 
 int main()
 {
-	GLFWwindow* window;
-	if (glfwInit() == GLFW_FALSE)
-		return -1;
-
-	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
-	window = glfwCreateWindow(INITIAL_WIDTH, INITIAL_HEIGHT, "Shape-Drawer", NULL, NULL);
-	if (window == NULL)
 	{
-		std::cout << "Could not create a window\n";
+		GLFWwindow* window;
+		if (glfwInit() == GLFW_FALSE)
+			return -1;
+
+		glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+		window = glfwCreateWindow(INITIAL_WIDTH, INITIAL_HEIGHT, "Shape-Drawer", NULL, NULL);
+		if (window == NULL)
+		{
+			std::cout << "Could not create a window\n";
+			glfwTerminate();
+			return -1;
+		}
+
+		glfwMakeContextCurrent(window);
+
+		setProjMatrix(INITIAL_WIDTH, INITIAL_HEIGHT);
+
+		constexpr size_t MINIMUM_SHAPE_NUM = 20;
+		constexpr size_t MAXIMUM_SHAPE_NUM = 1'000;
+		constexpr size_t CHANGE_SHAPE_NUM_STEP = 10;
+		int32_t random_scene_each_shape_num = MINIMUM_SHAPE_NUM;
+
+		Scene test_scene = makeTestScene();
+		Scene random_scene = makeRandomScene(random_scene_each_shape_num, INITIAL_WIDTH, INITIAL_HEIGHT);
+		Scene* current_scene_ptr = &test_scene;
+
+		KeyStates key_states{};
+		glfwSetWindowUserPointer(window, &key_states);
+
+		glfwSetKeyCallback(window, keyCallback);
+
+		while (!glfwWindowShouldClose(window))
+		{
+			/* RENDER BEGIN */
+			glClearColor(0.15f, 0.15f, 0.16f, 1.0f);
+			glClear(GL_COLOR_BUFFER_BIT);
+			current_scene_ptr->drawScene();
+			/* RENDER END */
+
+			if (key_states.switch_to_test_scene) {
+				current_scene_ptr = &test_scene;
+				key_states.switch_to_test_scene = false;
+			}
+			else if (key_states.switch_to_random_scene) {
+				random_scene = makeRandomScene(random_scene_each_shape_num, INITIAL_WIDTH, INITIAL_HEIGHT);
+				current_scene_ptr = &random_scene;
+				key_states.switch_to_random_scene = false;
+			}
+
+			if (key_states.increase_shape_num_in_random_scene) {
+				random_scene_each_shape_num += CHANGE_SHAPE_NUM_STEP;
+
+				if (random_scene_each_shape_num > MAXIMUM_SHAPE_NUM) {
+					random_scene_each_shape_num = MAXIMUM_SHAPE_NUM;
+				}
+				else if (current_scene_ptr == &random_scene) {
+					// if the random scene is currently selected - reinitialize it
+					// otherwise - postpone creating until scene is requested
+					random_scene = makeRandomScene(random_scene_each_shape_num, INITIAL_WIDTH, INITIAL_HEIGHT);
+				}
+
+				key_states.increase_shape_num_in_random_scene = false;
+			}
+			else if (key_states.decrease_shape_num_in_random_scene) {
+				random_scene_each_shape_num -= CHANGE_SHAPE_NUM_STEP;
+
+				if (random_scene_each_shape_num < MINIMUM_SHAPE_NUM) {
+					random_scene_each_shape_num = MINIMUM_SHAPE_NUM;
+				}
+				else if (current_scene_ptr == &random_scene) {
+					// if the random scene is currently selected - reinitialize it
+					// otherwise - postpone creating until scene is requested
+					random_scene = makeRandomScene(random_scene_each_shape_num, INITIAL_WIDTH, INITIAL_HEIGHT);
+				}
+
+				key_states.decrease_shape_num_in_random_scene = false;
+			}
+
+			glfwSwapBuffers(window);
+
+			glfwPollEvents();
+		}
+
 		glfwTerminate();
-		return -1;
 	}
 
-	glfwMakeContextCurrent(window);
-
-	setProjMatrix(INITIAL_WIDTH, INITIAL_HEIGHT);
-
-	constexpr size_t MINIMUM_SHAPE_NUM = 20;
-	constexpr size_t MAXIMUM_SHAPE_NUM = 1'000;
-	constexpr size_t CHANGE_SHAPE_NUM_STEP = 10;
-	int32_t random_scene_each_shape_num = MINIMUM_SHAPE_NUM;
-
-	Scene test_scene = makeTestScene();
-	Scene random_scene = makeRandomScene(random_scene_each_shape_num, INITIAL_WIDTH, INITIAL_HEIGHT);
-	Scene* current_scene_ptr = &test_scene;
-
-	KeyStates key_states{};
-	glfwSetWindowUserPointer(window, &key_states);
-
-	glfwSetKeyCallback(window, keyCallback);
-
-	while (!glfwWindowShouldClose(window))
-	{
-		/* RENDER BEGIN */
-		glClearColor(0.15f, 0.15f, 0.16f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
-		current_scene_ptr->drawScene();
-		/* RENDER END */
-
-		if (key_states.switch_to_test_scene) {
-			current_scene_ptr = &test_scene;
-			key_states.switch_to_test_scene = false;
-		}
-		else if (key_states.switch_to_random_scene) {
-			random_scene = makeRandomScene(random_scene_each_shape_num, INITIAL_WIDTH, INITIAL_HEIGHT);
-			current_scene_ptr = &random_scene;
-			key_states.switch_to_random_scene = false;
-		}
-
-		if (key_states.increase_shape_num_in_random_scene) {
-			random_scene_each_shape_num += CHANGE_SHAPE_NUM_STEP;
-
-			if (random_scene_each_shape_num > MAXIMUM_SHAPE_NUM) {
-				random_scene_each_shape_num = MAXIMUM_SHAPE_NUM;
-			} 
-			else if (current_scene_ptr == &random_scene) {
-				// if the random scene is currently selected - reinitialize it
-				// otherwise - postpone creating until scene is requested
-				random_scene = makeRandomScene(random_scene_each_shape_num, INITIAL_WIDTH, INITIAL_HEIGHT);
-			}
-
-			key_states.increase_shape_num_in_random_scene = false;
-		}
-		else if (key_states.decrease_shape_num_in_random_scene) {
-			random_scene_each_shape_num -= CHANGE_SHAPE_NUM_STEP;
-			
-			if (random_scene_each_shape_num < MINIMUM_SHAPE_NUM) {
-				random_scene_each_shape_num = MINIMUM_SHAPE_NUM;
-			} 
-			else if (current_scene_ptr == &random_scene) {
-				// if the random scene is currently selected - reinitialize it
-				// otherwise - postpone creating until scene is requested
-				random_scene = makeRandomScene(random_scene_each_shape_num, INITIAL_WIDTH, INITIAL_HEIGHT);
-			}
-
-			key_states.decrease_shape_num_in_random_scene = false;
-		}
-
-		glfwSwapBuffers(window);
-
-		glfwPollEvents();
-	}
-
-	glfwTerminate();
+	std::cout << "Memory used in the end:\n";
+	printMemoryUsage();
+	std::cin.get();
 	return 0;
 }
